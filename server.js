3344a864cbd27c31c38b5f7120b479b7f7962047
Juliana Dutra Moreira <JDM_JU@HOTMAIL.COM>
2023-11-26 14:11:56 -0300

    Ajustes para receber requisições de gravação e outros

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 // JSON Server module
 const jsonServer = require("json-server");
+const cors = require("cors");
+const {openSync} = require("fs");
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 
 // Make sure to use the default middleware
 const middlewares = jsonServer.defaults();
 
+server.use(cors());
+server.use(middlewares);
 server.use(middlewares);
 // Add this before server.use(router)
 server.use(
@@ -16,9 +20,13 @@ server.use(
 );
 server.use(router);
 // Listen to port
-server.listen(3000, () => {
-    console.log("JSON Server is running");
-});
+const PORT = process.env.PORT || 3000;
+
+openSync('db.json', 'r+');
 
 // Export the Server API
 module.exports = server;
+
+server.listen(PORT, () => {
+    console.log(`JSON Server is running on port ${PORT}`);
+});
